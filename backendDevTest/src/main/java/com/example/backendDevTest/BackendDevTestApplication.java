792e86e581991320b00e1826340e49b8cb24fd9b
Victor Marrero Carrillo <victortenemc@gmail.com>
2025-11-02 19:33:32 +0000

    Añade proyecto Spring Boot

---
+++ backendDevTest/src/main/java/com/example/backendDevTest/BackendDevTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendDevTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDevTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDevTestApplication.class, args);
+	}
+
+}
---
+++ backendDevTest/src/test/java/com/example/backendDevTest/BackendDevTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendDevTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDevTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
